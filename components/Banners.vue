361c8645e046086cb1ff7e020fff9bce731840db
Romeo <rommar@jpj.com>
2021-08-30 03:23:46 +0800

    Remove nuxt-link at banners

--- components/Banners.vue
+++ components/Banners.vue
@@ -2,20 +2,20 @@
   <div class="banners container">
     <ul class="row list-style-none">
       <li class="col-md-8">
-        <nuxt-link to="/featured/august-2021-promos">
+        <a href="/featured/august-2021-promos">
           <img
             title="Jomigu August 2021 Promos"
             src="~/assets/images/banners/jomigu-august-2021-promo_featured.png"
           />
-        </nuxt-link>
+        </a>
       </li>
       <li class="col-md-4">
-        <nuxt-link to="/categories/dbefabad-c4be-4a4b-aeed-79feb1c6c517">
+        <a href="/categories/dbefabad-c4be-4a4b-aeed-79feb1c6c517">
           <img
             title="Home School Kits"
             src="~/assets/images/banners/featuring_home-school-kits.png"
           />
-        </nuxt-link>
+        </a>
       </li>
     </ul>
   </div>
