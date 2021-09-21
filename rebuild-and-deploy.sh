29d1db9786317524a1247f88a2d8516eae267e30
Romeo <rommar@jpj.com>
2021-09-22 02:02:56 +0800

    Updated rebuild-and-deploy.sh

--- rebuild-and-deploy.sh
+++ rebuild-and-deploy.sh
@@ -1,12 +1,15 @@
 ! /bin/sh
 
 git pull && \
+
+mkdir -p $HOME/.jomigu && touch $HOME/.jomigu/build-start && date > $HOME/.jomigu/build-start
+
 npm run generate && \
 
 echo "Rebuild complete!"
 
 npm run deploy
 
-mkdir -p $HOME/.jomigu && touch $HOME/.jomigu/build && date > $HOME/.jomigu/build
+mkdir -p $HOME/.jomigu && touch $HOME/.jomigu/build-end && date > $HOME/.jomigu/build-end
 
 echo "Deployed!"
