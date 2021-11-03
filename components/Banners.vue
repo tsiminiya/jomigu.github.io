a091ae7cb29ac394d48eeec4dd99458f2bb75ead
Romeo <rommar@jpj.com>
2021-11-04 01:36:58 +0800

    Updated Banners

--- components/Banners.vue
+++ components/Banners.vue
@@ -2,10 +2,10 @@
   <div class="banners container">
     <ul class="row list-style-none">
       <li class="col-md-8">
-        <a href="/products/ad12e553-b0ae-4e2b-aba8-fa658ca40140">
+        <a href="/categories/2d45aac8-2d54-4a5b-a9f3-1b73ce57510e">
           <img
-            title="Custom YL Sticker Promo"
-            src="~/assets/images/banners/custom-yl-stickers-promo-september-2021.png"
+            title="YL Bottle Cap Label Stickers"
+            src="~/assets/images/banners/yl-most-wanted-and-top-10-stickers_sharing.png"
           />
         </a>
       </li>
@@ -13,7 +13,15 @@
         <a href="/products/5ac258d0-6f96-46ab-92c5-2c9b7c601383">
           <img
             title="Best Seller Books"
-            src="~/assets/images/banners/jomigu-budget-planner-2021.png"
+            src="~/assets/images/banners/try-my-budget-planner_sharing.png"
+          />
+        </a>
+      </li>
+      <li class="col-md-12">
+        <a href="/categories/0229bef0-bef5-4c2b-80e2-aa5c5d31f174">
+          <img
+            title="Just for you"
+            src="~/assets/images/banners/just-for-you-jomigu_sharing.png"
           />
         </a>
       </li>
