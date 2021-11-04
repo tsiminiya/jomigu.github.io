8db68d60256c1bd451810502d0862087addef0a3
Romeo <rommar@jpj.com>
2021-11-04 15:03:37 +0800

    Adjust position of banners

--- components/Banners.vue
+++ components/Banners.vue
@@ -2,10 +2,10 @@
   <div class="banners container">
     <ul class="row list-style-none">
       <li class="col-md-8">
-        <a href="/categories/2d45aac8-2d54-4a5b-a9f3-1b73ce57510e">
+        <a href="/categories/0229bef0-bef5-4c2b-80e2-aa5c5d31f174">
           <img
-            title="YL Bottle Cap Label Stickers"
-            src="~/assets/images/banners/yl-most-wanted-and-top-10-stickers_sharing.png"
+            title="Just for you"
+            src="~/assets/images/banners/just-for-you-jomigu_sharing.png"
           />
         </a>
       </li>
@@ -18,10 +18,10 @@
         </a>
       </li>
       <li class="col-md-12">
-        <a href="/categories/0229bef0-bef5-4c2b-80e2-aa5c5d31f174">
+        <a href="/categories/2d45aac8-2d54-4a5b-a9f3-1b73ce57510e">
           <img
-            title="Just for you"
-            src="~/assets/images/banners/just-for-you-jomigu_sharing.png"
+            title="YL Bottle Cap Label Stickers"
+            src="~/assets/images/banners/yl-most-wanted-and-top-10-stickers_sharing.png"
           />
         </a>
       </li>
