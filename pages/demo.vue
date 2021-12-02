59a0c8c4d18a3b15fb45497ae30f20b2d5bfc010
Romeo <rommar@jpj.com>
2021-12-02 17:13:34 +0800

    Updated product stats

---
+++ pages/demo.vue
@@ -0,0 +1,7 @@
+<template>
+  <iframe
+    src="https://script.google.com/macros/s/AKfycbwLG8_2AeY9K2eDqtiFE6SEK8xE0-rmCbd2IdCj22okNWUNkHTi12E6JbVGZ8iuQEUQAQ/exec/test/dmario"
+    onload='javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));'
+    style="height: 200px; width: 100%; border: none; overflow: hidden"
+  ></iframe>
+</template>
